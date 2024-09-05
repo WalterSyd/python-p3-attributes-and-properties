3d95a6310e668297efe0e8b5f6c941f6cf522c4f
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-05 06:40:27 +0300

    Described Dog class as per instructions

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,27 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Guok"):
+        # Initialize a private variable
+        self._name = None
+        # Use the setter to validate the name
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):        
+        #a built-in Python function that checks if an object is an instance of a particular class.
+        if isinstance(value, str) and 1 < len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+dog = Dog("Snoopy")
+print(dog.name)  
+
+
+dog.name = "A really long name that exceeds the limit"  # Output: Name must be string between 1 and 25 characters.
+print(dog.name)  
