31b3e331674197cd47fa05a853db17153e76d31f
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-05 07:08:39 +0300

    Implemented name functionality in person.py

--- lib/dog.py
+++ lib/dog.py
@@ -12,12 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    def __init__(self, name="Guok"):
-        # Initialize a private variable
+    def __init__(self, name="Guok", breed="Mastiff"):
+        # Initialize private variables for getter & setter methods
         self._name = None
+        self._breed = None
         # Use the setter to validate the name
         self.name = name
+        self.name = name
+        
     
+
+#Get setter for name
     @property
     def name(self):
         return self._name
@@ -30,9 +35,24 @@ class Dog:
         else:
             print("Name must be string between 1 and 25 characters.")
 
-dog = Dog("Snoopy")
-print(dog.name)  
+
+#Get setter for breed
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+dog = Dog("Snoopy", "Beagle")
+print(dog.name) 
+print(dog.breed) 
 
 
 dog.name = "A really long name that exceeds the limit"  # Output: Name must be string between 1 and 25 characters.
 print(dog.name)  
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,20 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Walt"):
+        self._name = None
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 < len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+person1 = Person("Onyi")
+print(person1.name)
